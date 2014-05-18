18b5fa123252a9367414ce75632cee1188a95cdd
unknown-user <unknown-user@192.168.0.18>
2014-05-18 02:53:47 +0100

    Initial check-in of module NA.AB.Remittance

---
+++ RemittanceAdvice/RemittanceAdvice/MainViewController.cs
@@ -0,0 +1,84 @@
+﻿using MonoTouch.UIKit;
+using System;
+using MonoTouch.Foundation;
+using System.Drawing;
+
+namespace RemittanceAdvice
+{
+	public partial class MainViewController : UIViewController
+	{
+		const float JVFieldHeight = 44.0f;
+		const float JVFieldHMargin = 10.0f;
+		const float JVFieldFontSize = 16.0f;
+		const float JVFieldFloatingLabelFontSize = 11.0f;
+
+		public MainViewController (IntPtr handle) : base (handle)
+		{
+			// Custom initialization
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+	
+			base.ViewDidLoad ();
+			Title = "Aktif Transfer";
+			View.BackgroundColor = UIColor.White;
+			/*
+			float topOffset = UIApplication.SharedApplication.StatusBarFrame.Size.Height; //+ NavigationController.NavigationBar.Frame.Size.Height;
+			UIColor floatingLabelColor = UIColor.Gray, floatingLabelActiveColor = UIColor.Blue;
+
+			PayTextField = new JVFloatLabeledTextField(new RectangleF(JVFieldHMargin, topOffset, 
+				View.Frame.Size.Width - 2 * JVFieldHMargin, 
+				JVFieldHeight))
+			{
+				Placeholder = "YEARLY CONTRACT REVENUE: *",
+				Font = UIFont.SystemFontOfSize(JVFieldFontSize),
+				ClearButtonMode = UITextFieldViewMode.WhileEditing,
+				FloatingLabelFont = UIFont.BoldSystemFontOfSize(JVFieldFloatingLabelFontSize),
+				FloatingLabelTextColor = floatingLabelColor,
+				FloatingLabelActiveTextColor = floatingLabelActiveColor,
+				KeyboardType = UIKeyboardType.DecimalPad
+			};
+			View.AddSubview(PayTextField);
+			*/
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+
+		partial void showInfo (NSObject sender)
+		{
+		}
+	}
+}
+
---
+++ RemittanceAdvice/RemittanceAdvice/MainViewController.designer.cs
@@ -0,0 +1,30 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+using MonoTouch.UIKit;
+
+namespace RemittanceAdvice
+{
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Outlet]
+		UITextField PayTextField { get; set; }
+
+		[Action ("showInfo:")]
+		partial void showInfo (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (PayTextField != null) {
+				PayTextField.Dispose ();
+				PayTextField = null;
+			}
+		}
+	}
+}
