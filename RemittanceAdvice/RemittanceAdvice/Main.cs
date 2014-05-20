100faf13ccec893cd028948cec4abe5f427e63a3
datax0 <datax0@me.com>
2014-05-20 23:48:23 +0100

    Tabcontrollers item added to Storyboard

--- RemittanceAdvice/RemittanceAdvice/Main.cs
+++ RemittanceAdvice/RemittanceAdvice/Main.cs
@@ -12,11 +12,13 @@ namespace RemittanceAdvice
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
+			PixateFreestyleLib.PixateFreestyle.InitializePixateFreestyle ();
 			//Setup our services for core library
 			ServiceRegistrar.Startup ();
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
+
 		}
 	}
 }
